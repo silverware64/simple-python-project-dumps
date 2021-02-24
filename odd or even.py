c4a8a0bd38fd53901ccb5a49f64aa13dec34318a
silverware64 <75706588+silverware64@users.noreply.github.com>
2021-02-24 04:44:07 -0700

    Add files via upload

---
+++ odd or even.py
@@ -0,0 +1,7 @@
+#is your number odd or even????
+num = int(input("Enter anyyyyyyyyyyyyy number ... just one: "))
+#smh indents and no brackets
+if (num %2) == 0:
+    print("{0} is EVEN".format(num))
+else:
+    print("{0} is ODD".format(num))
